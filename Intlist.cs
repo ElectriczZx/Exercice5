c5ab1663b918bac380c0cff3b88acc391152da8a
Nico Isbarn <n.isbarn17@gmail.com>
2020-10-24 17:17:13 +0200

    Add project files.

---
+++ Intlist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice5
+{
+    class Intlist
+    {
+        List<int> intList = new List<int>();
+
+
+        public Intlist()
+        {
+            intList.Add(5);
+            intList.Add(1);
+            intList.Add(3);
+            intList.Add(2);
+            intList.Add(8);
+        }
+
+        public void setList(int x, int y)
+        {
+                if (x <= intList.Count+1 & x>0)
+                {
+                    intList.Insert(x-1, y);
+                }
+                else
+                {
+                Console.WriteLine("Nope.");
+                }
+            }
+        
+
+        public void printList()
+        {
+            foreach(int amount in intList)
+            {
+                Console.WriteLine("Number: "+amount);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            int pos;
+
+            Intlist il = new Intlist();
+
+            Console.Write("Value: ");
+            value = int.Parse(Console.ReadLine());
+            Console.Write("Position: ");
+            pos = int.Parse(Console.ReadLine());
+            il.setList(pos, value);
+            il.printList();
+        }
+    }
+}
